a1a6be905182a2194ea97355c645dabef05e2388
Mike Kroutikov <pgmmpk@gmail.com>
2016-03-20 16:55:44 -0400

    changed generator of single_patterns.txt following the spirit of Aleksander's manual work.

--- hyphenation/make_pats.py
+++ hyphenation/make_pats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -97,43 +98,48 @@ CU_COMB_TITLO_RIGHT = '\ufe2f'
 CU_COMB_CONJ_MACRON = '\ufe26'
 
 CU_VOWELS = [
-    '\u0430',
-    '\u0435',
-    '\u0438',
-    '\u0439',
-    '\u043e',
-    '\u0443',
-    '\u044b',
-    '\u044d',
-    '\u044e',
-    '\u044f',
-    '\u0450',
-    '\u0454',
-    '\u0456',
-    '\u0457',
-    '\u0458',
-    '\u0461',
-    '\u0463',
-    '\u0465',
-    '\u0467',
-    '\u0469',
-    '\u046b',
-    '\u046d',
-    '\u0475',
-    '\u0477',
-    '\u047b',
-    '\u047d',
-    '\u047f',
-    '\ua657',
-    '\ua659',
-    '\ua65b',
-    '\ua65f',
-    '\ua65d',
+    '\u0430',  # а
+    '\u0435',  # е
+    '\u0454',  # є
+    '\u0438',  # и
+    '\u0439',  # й
+    '\u0456',  # i
+    '\u043e',  # о
+    '\u1c82',  # sharp o
+    '\u047b',  # ѻ
+    '\u0461',  # ѡ
+    '\u047d',  # ѽ
+    '\u047f',  # ѽ
+    '\ua64d',  # ꙍ
+    '\u044b',  # ы
+    '\u044d',  # э
+    '\u0463',  # ѣ
+    '\u044e',  # ю
+    '\u0467',  # ѧ
+    '\ua657',  # ꙗ
+    '\u0475',  # ѵ
+    '\u0443',  # у
+    '\ua64b',  # ꙋ
+    '\u0479',  # ѹ
 ]
 
 CU_BREATHING = '\u0486'
 CU_BREATHING_HARD = '\u0485'
 
+CU_ACCENTS_1 = [  # Aleksandr put only those for the start of the word patterns
+    '\u0300',
+    '\u0301',
+    '\u0311',
+    '\u0308'
+]
+
+CU_ACCENTS_2 = [
+    unichr(x) for x in range(ord('\u2de0'), ord('\u2dff')+1)
+] + [
+    unichr(x) for x in range(ord('\ua674'), ord('\ua67d')+1)
+]
+
+CU_POKRYTIE = 'u0487'
 
 def main_combining(args):
 
@@ -141,16 +147,20 @@ def main_combining(args):
 
     with codecs.open(args.output, 'w', 'utf-8') as f:
         f.write('% Combiners and related special patterns\n')
+
         f.write(inh.join(CU_UK_NFD)
                 + '  % Do not split digraph `LOWERCASE UK` with a hyphen: `U+1C82` `U+0443`\n')
         f.write(inh.join(CU_UK_NFD2)
                 + '  % Do not split this gorm of digraph `LOWERCASE UK` with a hyphen: `U+043e` `U+0443`\n')
         f.write('% combiners: never split before any combining symbol\n')
+
         for x in CU_COMBINERS:
             f.write(inh + x + '\n')
         f.write('%\n')
+        
         for x in (CU_YER, CU_YEROK, CU_TALL_YER, CU_SOFT_SIGN):
             f.write(inh + x + '\n')
+        
         f.write('% do not hyphenate before nor after those:\n')
         for x in (CU_PAEROK, CU_KAVYKA, CU_COMB_TITLO_LEFT, CU_COMB_TITLO_RIGHT, CU_COMB_CONJ_MACRON):
             f.write(inh + x + inh + '\n')
@@ -167,18 +177,23 @@ def main_singles(args):
             if x in (CU_UK, CU_OT):
                 continue
             f.write('.' + x + inh + '\n')
-            for y in CU_COMBINERS:
+            for y in CU_ACCENTS_1:
                 f.write('.' + x + y + inh + '\n')
                 f.write('.' + x + CU_BREATHING + y + inh +'\n')
-                f.write('.' + x + CU_BREATHING_HARD + y + inh +'\n')
+            for y in CU_ACCENTS_2:
+                f.write('.' + x + y + inh + '.\n')
+                f.write('.' + x + y + CU_POKRYTIE + inh + '.\n')
         f.write('%\n')
+
         f.write('% Forbid hyphenation before a single letter (with all possible and impossible combiners!)\n')
         for x in CU_VOWELS:
             f.write(inh + x + '.\n')
-            for y in CU_COMBINERS:
+            for y in CU_ACCENTS_1:
+                f.write('.' + x + y + inh + '\n')
+                f.write('.' + x + CU_BREATHING + y + inh +'\n')
+            for y in CU_ACCENTS_2:
                 f.write(inh + x + y + '.\n')
-                f.write(inh + x + CU_BREATHING + y +'.\n')
-                f.write(inh + x + CU_BREATHING_HARD + y +'.\n')
+                f.write(inh + x + y + CU_POKRYTIE + '.\n')
         f.write('% end single-letter rules\n')
 
 
